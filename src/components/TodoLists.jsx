158825353ab83a00047dbc8c8fb48db93e294387
seonyeonghun <seonyeonghun@kakao.com>
2025-03-05 12:08:04 +0900

    todoLists 컴포넌트 스타일 추가

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -1,9 +1,34 @@
-function TodoLists(){
-    return (
-      <ul>
-        <li>할일 목록이 여기에 표시됩니다.</li>
-      </ul>
-    )
+import "./TodoLists.css";
+function TodoLists({ todos }) {
+  function convertDday(todo) {
+    return Math.round(
+      (new Date(todo.end).getTime() - new Date(todo.start).getTime()) /
+        (1000 * 60 * 60 * 24)
+    );
   }
+  function convertDate(todo) {
+    return new Date(todo.end).toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+  }
+  return (
+    <ul className="items">
+      {todos.map((todo) => (
+        <li key={todo.id} className="item">
+          <div className="main">
+            <h2 className="text-primary">{todo.title}</h2>
+            <p className="text-secondary">{todo.desc}</p>
+          </div>
+          <div className="extra">
+            <span className="d-day">D - {convertDday(todo)}</span>
+            <span className="end-date">{convertDate(todo)}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
-  export default TodoLists;
+export default TodoLists;
