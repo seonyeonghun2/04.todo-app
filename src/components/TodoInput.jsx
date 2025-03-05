12418e3ba22f71cf4f30aebfc8f9e4a4667f7f39
seonyeonghun <seonyeonghun@kakao.com>
2025-03-05 10:59:31 +0900

    props 변경, 이벤트 핸들러 변경, name 속성 추가

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,15 +1,14 @@
-function TodoInput({startDate, endDate, title, onTitleChange, description, onDescChange, onStartDateChange, onEndDateChange, onSubmit}) {
-    console.log("넘어온 title : ", title)
+function TodoInput({formData,onChange, onSubmit}) {
     return (
       <form onSubmit={onSubmit}>
         <fieldset>
           <legend>Todo App</legend>
           <div className="container d-flex flex-column gap-1">
-            <input type="text" value={title} onChange={(e) => onTitleChange(e.target.value)} />
-            <textarea rows="5" cols="20" placeholder="add a description..." value={description} onChange={(e) => onDescChange(e.target.value)}></textarea>
+            <input type="text" name="title" value={formData.title} onChange={onChange} placeholder="title here" />
+            <textarea rows="5" name="desc" cols="20" placeholder="add a description..." value={formData.desc} onChange={onChange}></textarea>
             <div className="d-flex gap-1 align-items-center justify-content-between">
-                <label htmlFor="start-date">시작일</label><input type="date" id="start-date" value={startDate} onChange={(e) => onStartDateChange(e.target.value) } className="m-0" />
-                <label htmlFor="end-date">종료일</label><input type="date" id="end-end" value={endDate} onChange={(e) => onEndDateChange(e.target.value) } className="m-0" />
+                <label htmlFor="start-date">시작일</label><input type="date" id="start-date" name="start" value={formData.start} onChange={onChange} className="m-0" />
+                <label htmlFor="end-date">종료일</label><input type="date" id="end-end" name="end" value={formData.end} onChange={onChange} className="m-0" />
             </div>
             <input type="submit" value="등록" />
           </div>
