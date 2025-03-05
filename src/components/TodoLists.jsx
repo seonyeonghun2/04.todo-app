66b50500e3a55bf37efa61c1acd2f79f6f7cc749
seonyeonghun <seonyeonghun@kakao.com>
2025-03-05 16:10:26 +0900

    TodoLists 컴포넌트 | classnames 모듈 적용

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -1,6 +1,7 @@
+import classNames from "classnames";
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "./TodoLists.css";
-function TodoLists({ todos }) {
+function TodoLists({ todos }) {  
   function convertDday(todo) {
     return Math.round(
       (new Date(todo.end).getTime() - new Date(todo.start).getTime()) /
@@ -18,17 +19,17 @@ function TodoLists({ todos }) {
     <ul className="items">
       {todos.map((todo) => (
         <li key={todo.id} className="item">
-          <h2 className="category">{todo.category}</h2>
-          <div className="main">
+          <h2 className={classNames("category", todo.color)}>{todo.category}</h2>
+          <div className={classNames("main", todo.color)}>
             <h2 className="text-primary text-overflow">{todo.title}</h2>
             <p className="text-secondary">{todo.desc}</p>
           </div>
           <div className="extra d-flex">
             <div className="d-flex gap-1">
-              <span className="d-day">D - {convertDday(todo)}</span>
-              <span className="end-date">{convertDate(todo)}</span>
+              <span className="d-day"><i className="bi bi-calendar-week"></i> D - {convertDday(todo)}</span>
+              <span className="end-date"><i className="bi bi-pin-angle"></i> {convertDate(todo)}</span>
             </div>
-            <span className="ml-1"><i class="bi bi-people"></i>{todo.participant}</span>
+            <span className="ml-1"><i className="bi bi-people"></i> {todo.participant}</span>
           </div>
         </li>
       ))}
